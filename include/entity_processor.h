505dbc93c7d4ea67ec107a2b00405435a87ae1fd
Rodrigo Causarano <rjcausarano@gmail.com>
2025-02-15 14:57:02 -0300

    EntityProcessor class to serialize and deserialize Entity data

---
+++ include/entity_processor.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "file.h"
+#include "../proto/dir_descriptor.pb.h"
+#include <string>
+
+class EntityProcessor{
+	public:
+	EntityProcessor(const File& file);
+  EntityProcessor(const Entity& entity);
+  EntityProcessor(const std::string serialized_data);
+	std::string serialize() const;
+  void printDescription() const;
+	
+	private:
+  void createNestedEntity(File parentFile, Entity* parentEntity) const;
+  void printNestedEntity(const Entity& entity, int depth = 0) const;
+  Entity entity_;
+};
--- test/test.cc
+++ test/test.cc
@@ -1,26 +1,15 @@
 #include <iostream>
 #include "file.h"
-#include "file_serializer.h"
-
-void printEntity(Entity& entity, int depth = 0){
-  std::string indent(depth * 2, ' ');
-  std::cout << indent << entity.name() << std::endl;
-  if(entity.type() == Entity::DIRECTORY){
-    for(Entity child : entity.entities()){
-      printEntity(child, depth + 1);
-    }
-  }
-}
+#include "entity_processor.h"
 
 int main(){
 	File file("../test/parent_dir");
-	FileSerializer fileSerializer(file);
-	std::string serialized_data = fileSerializer.serialize();
+	EntityProcessor entityProcessor(file);
+	std::string serialized_data = entityProcessor.serialize();
 
   // Now deserialize it and show the contents
 
-  Entity entity;
-  entity.ParseFromString(serialized_data);
-  printEntity(entity);
+  EntityProcessor newEntityProcessor(serialized_data);
+  newEntityProcessor.printDescription();
 	return 0;
 }
