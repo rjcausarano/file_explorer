590d7290bfaa4523ef85b71e11467349de55ccee
Rodrigo Causarano <rjcausarano@gmail.com>
2025-02-23 21:54:51 -0300

    fetching dirTree, with a bug

--- include/comms/socket_common.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define PROTOCOL 0
-#define PORT 54321
-#define BUFFER_SIZE 1024
-#define TRUE 1
-#define FALSE 0
---
+++ test/client_main.cc
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include "file.h"
+#include "entity_processor.h"
+#include "comms/communicator.h"
+
+int main(int argc, char const* argv[])
+{
+  if(argc < 2){
+    std::cout << "The server IP address must be passed as argument." << std::endl;
+    return 1;
+  }
+
+  Communicator communicator((std::string(argv[1])));
+  Entity entity = communicator.getDirTree(".");
+  EntityProcessor processor(entity);
+  processor.printDescription();
+  std::string tmp;
+  std::cin >> tmp;
+
+  return 0;
+}
